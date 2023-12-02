659d22c90e48e8fe43496a8d9e81e95d7f617bd2
MEstor189 <MEstor189>
2023-12-02 01:55:53 +0100

    jaa

--- Day1/day.java
+++ Day1/day.java
@@ -10,7 +10,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,128 +20,165 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class day {
+    private static String s;
 
-
-        public static List<String> readLinesFromFile(String filePath) {
-            List<String> lines = null;
-            Path path = Paths.get(filePath);
-            try {
-                lines = Files.readAllLines(path);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return lines;
+    public static List<String> readLinesFromFile(String filePath) {
+        List<String> lines = null;
+        Path path = Paths.get(filePath);
+        try {
+            lines = Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    //test
-    
-    // eightwo = 8wo 
-    // aber bei mir eigh2 
-    //weil erst aufsteigend geguckt wird und nicht von rechts nach links
-    
-    
-    
-    
-    public static ArrayList<String> filterNum(List<String> inputList) {     
+        return lines;
+    }
+    // test
+
+    // eightwo = 8wo
+    // aber bei mir eigh2
+    // weil erst aufsteigend geguckt wird und nicht von rechts nach links
+
+    public static ArrayList<String> filterNum(List<String> inputList) {
         ArrayList<String> alNum = new ArrayList<>();
         ArrayList<String> alNumNew = new ArrayList<>();
-        for(String line : inputList){
+        // System.out.println(inputList);
 
-            
-            
-            switch (line) {
-                case "one":
-                    line.replace("one", "1");
-                    break;
-                case "two":
-                    line.replace("two", "2");
-                    break;
-                case "three":
-                    line.replace("three", "3");
-                    break;
-                case "four":
-                    line.replace("four", "4");
-                    break;
-                case "five":
-                    line.replace("five", "5");
-                    break;
-                case "six":
-                    line.replace("six", "6");
-                    break;
-                case "seven":
-                    line.replace("seven", "7");
-                    break;
-                case "eight":
-                    line.replace("eight", "8");
-                    System.out.println("HELP");
-                    break;
-                case "nine":
-                    line.replace("nine", "9");
-                    break;
-            
-                default:
-                    break;
-            }
-         
-            System.out.println(line);
-            alNumNew.add(line);
+        for (String line : inputList) {
+            alNumNew.add(convertStringToNumber(line));
+            // System.out.println("OO");
 
         }
+        
         Pattern p = Pattern.compile("[+-]?[0-9]+");
-        String s="";
+        String s = "";
         for (String line : alNumNew) {
             Matcher m = p.matcher(line);
-            while ( m.find() ) {
-                int a=Integer.parseInt(line.substring(m.start(), m.end()));
-                s= s+line.substring(m.start(), m.end()); 	        
-    	}
-        alNum.add(s);
-        s="";
-        }   
+            while (m.find()) {
+                int a = Integer.parseInt(line.substring(m.start(), m.end()));
+                s = s + line.substring(m.start(), m.end());
+            }
+            alNum.add(s);
+            s = "";
+        }
 
-        //System.out.println(alNum);
+        // System.out.println(alNum);
         return alNum;
     }
 
-    public static Integer sumAll(ArrayList<String>numArrayList){
+
+    public static Integer sumAll(ArrayList<String> numArrayList) {
         char firstDigit;
         char lastDigit;
         String num;
-        int sum=0;
+        int sum = 0;
         for (String i : numArrayList) {
-            //System.out.println(i);
-            if(i.length()>2){
-                firstDigit =  i.charAt(0);
-                lastDigit = i.charAt(i.length()-1);
-                num= ""+firstDigit+lastDigit;
-                //System.out.println(num);
-                sum=sum+ Integer.parseInt(num);
-            }else if (i.length()<2) {
-                i=i+i;
-               // System.out.println(i);
-                sum=sum+Integer.parseInt(i);
-            }else{
-               // System.out.println(i);
-                sum=sum+Integer.parseInt(i);
+            // System.out.println(i);
+            if (i.length() > 2) {
+                firstDigit = i.charAt(0);
+                lastDigit = i.charAt(i.length() - 1);
+                num = "" + firstDigit + lastDigit;
+                 System.out.println(num);
+                sum = sum + Integer.parseInt(num);
+            } else if (i.length() < 2) {
+                i = i + i;
+                 System.out.println(i);
+                sum = sum + Integer.parseInt(i);
+            } else {
+                 System.out.println(i);
+                sum = sum + Integer.parseInt(i);
             }
 
-            //System.out.println(sum);
-            
         }
-
-        
-
 
         return sum;
     }
 
+
+
+
+
+
+
+
     public static void main(String[] args) {
         String filePath = "C:/Users/Siu/Desktop/aoc2023/d1.txt"; // Passe den Pfad entsprechend an
         List<String> lines = readLinesFromFile(filePath);
-
-        
-        filterNum(lines);
+        // System.out.println("l");
+        //filterNum(lines);
+        //System.out.println(convertStringToNumber("6seventntzffjkkvvhtgtwoonethreefivekzvptvxfjg"));
         System.out.println(sumAll(filterNum(lines)));
-        
+
+
     }
 
+    public static String convertStringToNumber(String inputString) {
+        List<String> nummern = new ArrayList<String>() {
+            {
+              add("one");
+              add("two");
+              add("three");
+              add("four");
+              add("five");
+              add("six");
+              add("seven");
+              add("eight");
+              add("nine");
+            }
+          };
+        Map<String, String> wordToNumberMap = new HashMap<>();
+        wordToNumberMap.put("one", "1");
+        wordToNumberMap.put("two", "2");
+        wordToNumberMap.put("three", "3");
+        wordToNumberMap.put("four", "4");
+        wordToNumberMap.put("five", "5");
+        wordToNumberMap.put("six", "6");
+        wordToNumberMap.put("seven", "7");
+        wordToNumberMap.put("eight", "8");
+        wordToNumberMap.put("nine", "9");
+
+        StringBuilder result = new StringBuilder();
+        StringBuilder currentNumber = new StringBuilder();
+        StringBuilder currentWordBuilder = new StringBuilder();
+
+        for (char c : inputString.toCharArray()) {
+            if (Character.isLetter(c)) {
+                String currentWord = currentNumber.toString();
+                currentNumber.setLength(0);
+
+                if (!currentWord.isEmpty()) {
+                    result.append(currentWord);
+                }
+
+                currentWordBuilder.append(c);
+
+
+                String currentWordString = currentWordBuilder.toString();
+                for (String string : nummern) {
+                    if(currentWordString.contains(string)){
+                        result.append(wordToNumberMap.get(string));
+                        currentWordBuilder=new StringBuilder();
+                    }
+                    
+                }
+
+                /*if (wordToNumberMap.containsKey(currentWordString)) {
+                    result.append(wordToNumberMap.get(currentWordString));
+                    currentWordBuilder= new StringBuilder();
+                }
+                */
+            } else if (Character.isDigit(c)) {
+                currentNumber.append(c);
+            }
+        }
+
+        // Füge die letzte Zahl hinzu
+        if (currentNumber.length() > 0) {
+            result.append(currentNumber);
+        }
+        //System.out.println(result.toString());
+        return result.toString();
+    }
+
+
 }
+
