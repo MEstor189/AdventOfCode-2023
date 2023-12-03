101576bbc4acd259c1cd5245c3fc1ed67a5db994
MEstor189 <MEstor189>
2023-12-03 15:05:41 +0100

    d3t2halb

--- Day3/day3.java
+++ Day3/day3.java
@@ -7,10 +7,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class day3 {
 
+    private static Map<String, List<Integer>> datenbank = new HashMap<>();
+    private static int x,y;
 
     public static ArrayList<char[]> readLines(String filePath) {
         ArrayList<char[]> linesList = new ArrayList<>();
@@ -28,11 +32,12 @@ public class day3 {
         return linesList;
     }
 
-    public static void vailid (ArrayList<char[]> lines){
+    public static int vailid (ArrayList<char[]> lines){
         String num=""; 
+        int sum=0;
         boolean valid = false;
         for(int i=0;i<lines.size();i++){
-            System.out.println(lines.get(i));
+           // System.out.println(lines.get(i));
              char [] line=lines.get(i);
             
             for (int j=0; j< line.length;j++) {
@@ -46,48 +51,152 @@ public class day3 {
 
                     //System.out.println();
                 }else if(num!=""){
-                    System.out.println(num + ": " +valid);
-                    //System.out.println(num);
+                    
+                    if(valid){
+
+                        stern(x, y, Integer.parseInt(num));
+
+                        sum= sum + Integer.parseInt(num);
+                        System.out.println(num);
+                    };
                     num = "";
                     valid=false;
                 }                
             }            
         } 
+        return sum;
     }
 
     public static boolean adjacent (ArrayList<char[]> lines, int i, int j, boolean valid){
         
         char [] currline = lines.get(i);
+        x=0;
+        y=0;
+
 
         if(j+1<currline.length){
 
         //rechts
-            if(!(currline[j+1]=='.'|| Character.isDigit(currline[j+1]))){
+            if((currline[j+1]=='*')){
                 valid = true;
+                x=j+1;
+                y=i;
             }
         }
         if(j-1>=0){   
-            if(!(currline[j-1]=='.'|| Character.isDigit(currline[j-1]))){
-                System.out.println(currline[j-1]);
+            if((currline[j-1]=='*')){
+                //System.out.println(currline[j-1]);
                 valid = true;   
+                x=j-1;
+                y=i;
             }
-        }    
+        } 
+        //oben
+        if(i-1>=0){
+            char[] bline = lines.get(i-1);
+            //direktoben
+            if((bline[j]=='*')){
+                valid = true;
+                x=j;
+                y=i-1;
+            }
+            if(j+1<bline.length){
+            //rechtsoben
+                if((bline[j+1]=='*')){
+                    valid = true;
+                    x=j+1;
+                    y=i-1;
+                }
+            }
+            if(j-1>=0){
+            //linksoben
+                if((bline[j-1]=='*')){
+                    valid = true;
+                    x=j-1;
+                    y=i-1;
+                }
+            }
+        }  
+        
+        // unten
+        if(i+1<lines.size()){
+
+            char[] aline = lines.get(i+1);
+            //direktunten
+            if((aline[j]=='*')){
+                valid=true;
+                x=j;
+                y=i+1;
+            }
+            //untenrechts
+            if(j+1<aline.length){
+            
+                if((aline[j+1]=='*')){
+                    valid = true;
+                    x=(j+1);
+                    y=(i+1);
+                    System.out.println(x+":"+y);
+                }
+            }
+            //untenlinks
+            if(j-1>=0){
+                if((aline[j-1]=='*')){
+                    valid = true;
+                    x=j-1;
+                    y=i+1;
+                }
+            }
+
+        }
+        
+
+
+
         return valid;   // .......421.............#..............+........*....493.......*..........589..................286...............254..........458...672...258
     }
+
+    public static void stern (int x, int y, int num){
+        String kombination = x + "&" + y;
+        if (datenbank.containsKey(kombination)) {
+            // Kombination vorhanden, Zahl hinzufügen
+            datenbank.get(kombination).add(num);
+        } else {
+            // Neue Kombination erstellen
+            List<Integer> zahlenListe = new ArrayList<>();
+            zahlenListe.add(num);
+            datenbank.put(kombination, zahlenListe);
+        }
+
+    }
+
+    public static void anzeigen() {
+        // Anzeigen der gespeicherten Daten
+        for (Map.Entry<String, List<Integer>> eintrag : datenbank.entrySet()) {
+            String kombination = eintrag.getKey();
+            List<Integer> zahlenListe = eintrag.getValue();
+
+            System.out.print(kombination + ": ");
+
+            // Alle Zahlen für die Kombination ausgeben
+            for (Integer zahl : zahlenListe) {
+                System.out.print(zahl + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+
 
     public static void main(String[] args) {
      
         String filePath = "C:/Users/Siu/Desktop/aoc2023/d3test.txt"; // Passe den Pfad entsprechend an
         ArrayList<char[]> lines = readLines(filePath);
-        vailid(lines);
+        System.out.println(vailid(lines));
+        anzeigen();
 
         // Print the content of the ArrayList
-        for (char[] line : lines) {
-            for (char ch : line) {
-               // System.out.print(ch + " ");
-            }
-           // System.out.println();
-        }
+     
     }
 }
         
