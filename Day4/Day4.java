7a566c3a86494ce7e2288c10f6686b3bda8ac84e
hUpLhAWmygz7ro <malteestor@gmail.com>
2023-12-04 11:45:01 +0100

    d4t2done

--- Day4/Day4.java
+++ Day4/Day4.java
@@ -5,11 +5,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day4 {
-    
-        public static List<String> readLinesFromFile(String filePath) {
+    private static int[][] winsCount;
+
+    public static List<String> readLinesFromFile(String filePath) {
         List<String> lines = null;
         Path path = Paths.get(filePath);
         try {
@@ -20,54 +23,92 @@ public class Day4 {
         return lines;
     }
 
-    public static int aufteilen (String inputstString){
+    public static int aufteilen(String inputstString) {
         ArrayList<String[]> aList = new ArrayList<>();
-        int counter=0;
-        
-        String [] teilStrings = inputstString.split("[:|]");
-        //System.out.println(teilStrings[0]);
+        int counter = 0;
 
+        String[] teilStrings = inputstString.split("[:|]");
+        // System.out.println(teilStrings[0]);
 
-        for (int i = 1; i<=2;i++) {
-            String [] teil = teilStrings[i].split(" ");
-            
+        for (int i = 1; i <= 2; i++) {
+            String[] teil = teilStrings[i].split(" ");
+
             aList.add(teil);
         }
 
-        String [] a = aList.get(1);
-        
+        String[] a = aList.get(1);
+
         for (String string : aList.get(0)) {
 
-            if(!string.isEmpty()){
+            if (!string.isEmpty()) {
 
-            for (String string2 : aList.get(1)) {
-                if(string.equals(string2)&& string!=""){
-                    counter++;
+                for (String string2 : aList.get(1)) {
+                    if (string.equals(string2) && string != "") {
+                        counter++;
+                    }
+
                 }
-                
             }
-        }
-            
-            
-                
-            
-            
-        }
-        System.out.println((int)Math.pow(2, counter-1));
 
-        return (int)Math.pow(2, counter-1);
-        
+        }
+        // System.out.println(counter);
+
+        return (counter);
+
+    }
+
+    public static int[][] calcScratches(int[][] dArray, int size) {
+
+        for (int i = 0; i < size; i++) {
+            for (int l =0; l<size;l++){
+           // System.out.println("JKAHD"+dArray[0][l]+"|"+dArray[1][l]);
+            }
+            int winstemp = dArray[0][i];
+           // int counttemp = dArray[1][i];
+           // System.out.println("wins: "+ winstemp+"| count: "+ counttemp );
+            //System.out.println("KK       "+ dArray[0][4] + "|"+ dArray[1][4]);
+            for (int j = 0; j < winstemp; j++) {
+                
+                int counttemp = dArray[1][i];
+                //System.out.println("COunter: " + counttemp);
+                if (i + winstemp <= size) {
+                    
+                   // System.out.println("K" + dArray[0][i]);
+                }
+                dArray[1][i+j + 1] = dArray[1][i+j+1] + counttemp;
+                //System.out.println("HÄÄÄÄÄ" + counttemp +"|"+ counttemp);
+            }
+            
+
+        }
+        return dArray;
     }
 
     public static void main(String[] args) {
-        String filePath = "C:/Users/malte/OneDrive/Desktop/AOC2023/d4test.txt"; // Passe den Pfad entsprechend an
+        String filePath = "C:/Users/malte/OneDrive/Desktop/AOC2023/d4.txt"; // Passe den Pfad entsprechend an
         List<String> lines = readLinesFromFile(filePath);
-        int num=0;
-        for (String string : lines) {
-            num = num + aufteilen(string);
+        int scratchAll = 0;
+        winsCount = new int[2][lines.size()];
+        ArrayList<Integer> wins = new ArrayList<>();
+        for (int i = 0; i < lines.size(); i++) {
+            String string = lines.get(i);
+            winsCount[0][i] = aufteilen(string);
+            winsCount[1][i] = 1;
         }
-        System.out.println(num);
-    }
+        for (int j = 0; j < lines.size(); j++) {
+            System.out.println(winsCount[0][j] + "|" + winsCount[1][j]);
+        }
 
+        int[][] test = calcScratches(winsCount, lines.size());
+        System.out.println("IIIIIIIIIIIIIIIIIIIII");
+
+        for (int j = 0; j < lines.size(); j++) {
+            int num = test[1][j];
+            scratchAll= scratchAll+num;
+        }
+        System.out.println(scratchAll);
+        /*              
+        */
+    }
 
 }
