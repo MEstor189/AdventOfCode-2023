5aa5e0003e1c5a3d32ce0033e9cf7aa21a284355
hUpLhAWmygz7ro <malteestor@gmail.com>
2023-12-05 12:17:29 +0100

    arsch

--- Day5/day5.java
+++ Day5/day5.java
@@ -7,59 +7,121 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class day5 {
 
     public static void main(String[] args) {
-        int max = 0;
-        int min = 0;
+        long max = 0;
+        long min = 0;
+        long nearseed=9000000000L;
 
-        List<List<Integer>> seedToLocationMap = readDataFromFile("C:/Users/malte/OneDrive/Desktop/AOC2023/d5test.txt");
+        List<List<Long>> seedToLocationMap = readDataFromFile("C:/Users/malte/OneDrive/Desktop/AOC2023/d5.txt");
 
         // Ausgabe der eingelesenen Daten
-        for (List<Integer> dataList : seedToLocationMap) {
-
-            if (!dataList.isEmpty()) {
-
-                int tempmax = dataList.get(1) + dataList.get(2);
-                int tempmin = dataList.get(1);
-                if (tempmax > max) {
-                    max = tempmax;
-                }
-
-                if (tempmin < min) {
-                    min = tempmin;
-                }
-
-                // System.out.println(dataList.get(0));
-                for (int data : dataList) {
-                    System.out.print(data + " ");
-
+        List<Long> seedList = seedToLocationMap.get(0);
+        List<List<Long>> blocklist = new ArrayList<>();
+ 
+                
+        for (Long seed : seedList) {
+            /* 
+            for (List<Long> klist : seedToLocationMap) {
+                for (Long  p  : klist) {
+                    System.out.print(p+ " ");
                 }
                 System.out.println();
-            } else {
-                System.out.println();
-                System.out.println(min);
-                System.out.println(max);
+            }
+            */
+        //seedToLocationMap.remove(0);
+        //seedToLocationMap.remove(0);
 
-                max = 0;
-                min = 100;
+
+            for (List<Long> dataList : seedToLocationMap) {
+
+                if (!dataList.isEmpty()) {
+                    blocklist.add(dataList);
+                   /*  for (Long o : dataList) {
+                       System.out.println(o); 
+                    }
+                    */
+                    Long tempmax = dataList.get(1) + dataList.get(2);
+                    //System.out.println(tempmax);
+                    Long tempmin = dataList.get(1);
+                    if (tempmax > max) {
+                        max = tempmax;
+                    }
+
+                    if (tempmin < min) {
+                        min = tempmin;
+                    }
+
+                    // System.out.println(dataList.get(0));
+                    for (Long data : dataList) {
+                        System.out.print(data + " ");
+
+                    }
+                    System.out.println();
+                } else {
+                    System.out.println();
+                    if(min<=seed&&max>=seed){
+
+                        
+                        //System.out.println(min);
+                        //System.out.println(max);
+                        seed= berechnen(seed, blocklist);
+
+
+                    }else{
+                        blocklist= new ArrayList<>();
+                    }
+
+                    max = 0;
+                    min = 9000000000L;
+                }
+
+                // System.out.println(min);
+
             }
 
-            // System.out.println(min);
+            if(nearseed>(seed)){
+                nearseed=seed;
 
+            }
+
+            System.out.println("-----------------------------");
         }
-        System.out.println(min);
-        System.out.println(max);
+        System.out.println("Ende: "+ nearseed);
 
     }
 
-    public static List<List<Integer>> readDataFromFile(String filename) {
+    public static Long berechnen (long seed, List<List<Long>> blocklist){
+        Map<Long, Long> datenbank = new HashMap<>();
+
+        for (List<Long> list : blocklist) {
+            
+            for (long num : list) {
+                
+                for(long k =0; k<=list.get(2);k++){
+                    long key = list.get(1) + k;
+                    long value = list.get(0)+k;
+                    datenbank.put(key, value);
+                }
+            }
+        }
+        System.out.println(datenbank.toString());
+        long test = datenbank.get(seed);
+        System.out.println("new: "+test);
+        return test;
+
+    }
+
+    public static List<List<Long>> readDataFromFile(String filename) {
         // Initialisiere das Scanner-Objekt und die Liste für Daten
         Scanner scanner = null;
-        List<List<Integer>> dataList = new ArrayList<>();
+        List<List<Long>> dataList = new ArrayList<>();
 
         try {
             // Datei einlesen
@@ -72,18 +134,19 @@ public class day5 {
 
                 if (!line.isEmpty()) {
                     String[] tokens = line.split(" ");
+                    System.out.println(line);
 
                     // Konvertiere nur die Token, die Zahlen repräsentieren
-                    List<Integer> dataBlock = new ArrayList<>();
+                    List<Long> dataBlock = new ArrayList<>();
                     for (String token : tokens) {
                         try {
-                            int data = Integer.parseInt(token);
+                            Long data = Long.parseLong(token);
                             dataBlock.add(data);
                         } catch (NumberFormatException e) {
                             // Ignoriere Token, die nicht in Zahlen umgewandelt werden können
                         }
                     }
-
+                   
                     // Füge den Datenblock zur Liste hinzu
                     dataList.add(dataBlock);
                 }
